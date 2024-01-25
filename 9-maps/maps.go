e9a136058b9754c21d1a436f01110b292386093f
netanmangal <imnetanmangal@gmail.com>
2024-01-25 21:49:03 +0530

    Update: 9-maps

--- 9-maps/maps.go
+++ 9-maps/maps.go
@@ -3,15 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	// declaration of map
-	// // var m map[int8]string
-	// this creates a nil map, this can only be read from and not be written to.
+	m1 := map[int]string{}
+	m1[0], m1[1] = "hello", "world"
 
-	m := make(map[int]string)
+	fmt.Println("map m1 : ", m1)
+	fmt.Printf("type of map m1 : %T \n", m1)
+	fmt.Println("len of map m1 : ", len(m1))
 
-	m[0] = "User1"
-	m[1] = "User2"
-	m[2] = "User3"
+	m2 := map[int]string{3: "apple", 4: "banana", 5: "carrot", 6: "orange"}
+	fmt.Println("map m2 : ", m2)
 
-	fmt.Println("map: ", m)
+	delete(m2, 5)
+	fmt.Println("deleting key[5] from m2 : ", m2)
+
+	m2[7] = ""
+	fmt.Println("map m2 : ", m2)
+
+	_, present := m2[7]
+	fmt.Println("Optional return value 2 from m2[7] if key is present : ", present)
 }
+
